e24a6f28afdff86ea64f6b28e2feb9ffb9ecd881
Daniel Villajuan <dvillajuan@andreani.com>
2020-09-28 17:41:13 -0300

    push base

---
+++ PS.Template.AccessData/Context/UsuarioDBContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PS.Template.AccessData.cualquiera
+{
+    public partial class UsuarioDBContext : DbContext
+    {
+        public UsuarioDBContext()
+        {
+        }
+
+        public UsuarioDBContext(DbContextOptions<UsuarioDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source= DESKTOP-E96Q542; Initial Catalog= UsuarioDB; user=sa; password=rosario1803; Integrated Security= true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__645723A6EF59050A");
+
+                entity.Property(e => e.IdUsuario)
+                    .HasColumnName("idUsuario")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Apellido)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Dni)
+                    .IsRequired()
+                    .HasColumnName("DNI")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNac)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdCuenta).HasColumnName("idCuenta");
+
+                entity.Property(e => e.IdDireccion).HasColumnName("idDireccion");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PS.Template.Domain/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS.Template.AccessData.cualquiera
+{
+    public partial class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public string FechaNac { get; set; }
+        public int IdDireccion { get; set; }
+        public int IdCuenta { get; set; }
+    }
+}
